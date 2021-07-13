f694a0bf74474e17fcfea36532cb66c041cd4776
vipul1142 <vipuljha1142@gmail.com>
2021-07-13 16:23:22 +0530

    added functionality on generate color button

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 // const getElementById = (id) => document.getElementById(id);
 
+function getNewColorDiv() {
+  const newElement = document.createElement("div");
+  newElement.className = "pallete-color";
+  return newElement;
+}
+
 function decreamentNumberOfColors() {
   const valueElement = document.getElementById("numberOfColors");
-  let currentNumberOfColors = Number(valueElement.innerText);
+  const currentNumberOfColors = Number(valueElement.innerText);
   if (currentNumberOfColors > 3)
     valueElement.innerText = currentNumberOfColors - 1;
 }
 
 function increamentNumberOfColors() {
   const valueElement = document.getElementById("numberOfColors");
-  let currentNumberOfColors = Number(valueElement.innerText);
+  const currentNumberOfColors = Number(valueElement.innerText);
   if (currentNumberOfColors < 7)
     valueElement.innerText = currentNumberOfColors + 1;
+}
+function generatePallete() {
+  const valueElement = document.getElementById("numberOfColors");
+  const currentNumberOfColors = Number(valueElement.innerText);
+  const palleteDiv = document.getElementById("colorPallete");
+  palleteDiv.innerHTML = "";
+  for (let i = 0; i < currentNumberOfColors; i++) {
+    palleteDiv.appendChild(getNewColorDiv());
+  }
 }
